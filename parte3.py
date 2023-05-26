9ad78d997f756654413b775f0d4c4608c72a9828
Tales <santiagold116@gmail.com>
2023-05-26 13:01:41 -0300

    falta resolver algumas coisas

--- parte3.py
+++ parte3.py
@@ -1,43 +1,42 @@
 import multiprocessing
+import threading
 
-def processo_filho(processo_id, shm, num_processos):
-    print(f"Processo filho {processo_id} iniciado")
+def processo_filho(processo_id, shm, num_processos, barreira):
+    print(f"processo filho {processo_id} iniciado")
+    barreira.wait()  # Aguarda a barreira para sincronizar a inicialização dos processos filhos
 
-    for i in range(num_processos): #ciclo for até o tamanho do número de processos
+    for i in range(num_processos):
         mensagem = shm.get()
         if mensagem:
-            print(f"Processo filho {processo_id} recebeu a mensagem: {mensagem}")
+            print(f"processo filho {processo_id} recebeu a mensagem: {mensagem}")
             shm.task_done()
-            # verifica se todos os processos já leram a mensagem
             if shm.qsize() == 0:
-                print("A mensagem foi lida por todos os processos")
+                print("a mensagem foi lida por todos os processos")
                 break
 
 def main():
     num_processos = int(input("numero de processos a serem gerados? "))
-
     if num_processos <= 2:
-        print("digite um numero maior que 2 seu btl.")
-        main()
+        print("digite um numero maior que 2 seu btl")
         return
-
-    # cria a fila compartilhada para a troca de mensagens
+    
+    # Cria a fila compartilhada para a troca de mensagens
     shm = multiprocessing.JoinableQueue()
 
-    # cria os processos filhos
+    # Cria a barreira para sincronizar a inicialização dos processos filhos
+    barreira = threading.Barrier(num_processos)
+
+    # Cria os processos filhos
     processos = []
     for i in range(num_processos):
-        p = multiprocessing.Process(target=processo_filho, args=(i, shm, num_processos))
+        p = multiprocessing.Process(target=processo_filho, args=(i, shm, num_processos, barreira))
         p.start()
         processos.append(p)
-
-    print("Processo pai iniciado.")
-
-    # pega a entrada do usuário e envia para os processos filhos
+    print("processo pai iniciado")
     mensagem = input("qual a mensagem que vc quer enviar ")
     for i in range(num_processos):
         shm.put(mensagem)
-
-    # espera todos os processos filhos concluírem
     shm.join()
     print("processo pai finalizado.")
+# Chamando a função main
+main()
